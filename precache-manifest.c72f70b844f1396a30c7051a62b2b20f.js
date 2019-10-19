3233a94a357dad9c017afc84787bb8f9b146d27d
Cong Zhang <dancerphil1994@gmail.com>
2019-10-19 17:44:17 +0800

    Updates

---
+++ precache-manifest.c72f70b844f1396a30c7051a62b2b20f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e734ff4b38f171ae758dd6c88a14e0db",
+    "url": "/mine/index.html"
+  },
+  {
+    "revision": "f66f6328752b626701ce",
+    "url": "/mine/static/css/main.753f3887.chunk.css"
+  },
+  {
+    "revision": "711ba69f51bcd1ec447a",
+    "url": "/mine/static/js/2.f9c27847.chunk.js"
+  },
+  {
+    "revision": "f66f6328752b626701ce",
+    "url": "/mine/static/js/main.09a2414e.chunk.js"
+  },
+  {
+    "revision": "ebee37393ac50c82771b",
+    "url": "/mine/static/js/runtime-main.e524b4bb.js"
+  }
+]);
